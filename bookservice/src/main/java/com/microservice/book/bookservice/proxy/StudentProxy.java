20833f3312a21f0b710674d0d47a60c417654722
Darshan <Darshan@DESKTOP-KDB0RIC>
2018-12-05 23:30:15 +0530

    Update code for Load Balancing (Ribbon Client) - Local Server

--- bookservice/src/main/java/com/microservice/book/bookservice/proxy/StudentProxy.java
+++ bookservice/src/main/java/com/microservice/book/bookservice/proxy/StudentProxy.java
@@ -1,12 +1,14 @@
 package com.microservice.book.bookservice.proxy;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.microservice.book.bookservice.model.Student;
 
-@FeignClient(name = "studentservice", url = "localhost:8000")
+@FeignClient(name = "studentservice")
+@RibbonClient(name = "studentservice")
 public interface StudentProxy {
 
 	@GetMapping("/student")
